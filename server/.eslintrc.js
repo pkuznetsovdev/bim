d7c50afe46d905e4fc419510cf95a98a384c5026
pkuznetsovdev <kuznetsov.pavel@pm.me>
2023-12-25 16:04:55 +0400

    feat: fix eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,33 +1,34 @@
 // .eslintrc.js
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-        'airbnb-base',
-        'airbnb-typescript/base',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+      },
     ],
-    plugins: ['@typescript-eslint', 'prettier'],
-    parserOptions: {
-        project: `./tsconfig.json`,
-        ecmaVersion: 2018,
-        sourceType: 'module',
-    },
-    rules: {
-        'prettier/prettier': 'error',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        'import/extensions': [
-            'error',
-            'ignorePackages',
-            {
-                ts: 'never',
-            },
-        ],
-        "no-shadow": "off",
-        "@typescript-eslint/no-shadow": "warn",
-        "import/prefer-default-export": "warn",
-        "radix": "off",
-        "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-        "consistent-return": "off",
-    },
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": "warn",
+    "import/prefer-default-export": "off",
+    radix: "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "consistent-return": "off",
+  },
 };
